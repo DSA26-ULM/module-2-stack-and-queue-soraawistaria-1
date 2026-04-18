327344aece1e8ef9608f26c9ec0f1c59eb91a387
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-18 20:37:38 +0800

    membingugkan

--- prob1.cpp
+++ prob1.cpp
@@ -1,7 +1,25 @@
 #include "stack.h"
+#include <iostream>
+using namespace std;
 
 int main() {
     Stack s;
+    int N, x;
 
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> x;
+
+        if (x >= 0 && x < 100) {
+            data.push(&s, x);
+        } else if (x == "+") {
+            data.pop(&s);
+            
+        }
+    }
     
+    for (int i = 0; i < N; i++) {
+
+    }
+
 }
--- stack.cpp
+++ stack.cpp
@@ -1,28 +1,28 @@
 #include "stack.h"
 
-void init(Stack* s) {
-    *s->top = -1;
+void init(Stack* s) { 
+    *s->top = -1;  //jari di posisi -1
 }
 bool isEmpty(const Stack* s) {
-    return *s->top == -1;
+    return *s->top == -1;  //apakah kosong? (-1)
 }
 bool isFull(const Stack* s) {
-    return *s->top == MAX - 1;
+    return *s->top == MAX - 1;  //apakah full? (99)
 }
 void push(Stack* s, int value) {
-    if (!isFull(s)) {
-        (*s->top)++;
-        s->data[*s->top] = value;
+    if (!isFull(s)) {  //kalau gak full
+        (*s->top)++;  
+        s->data[*s->top] = value;  //tambah elemen
     }
 }
 void pop(Stack* s) {
-    if (!isEmpty(s)) {
-        (*s->top)--;
+    if (!isEmpty(s)) {  //kalau gak kosong
+        (*s->top)--; 
     }
 }
 int peek(const Stack* s) {
-    if (!isEmpty(s)) {
-        return s->data[*s->top];
+    if (!isEmpty(s)) {  //kalau gak kosong
+        return s->data[*s->top];  //intip elemen terbaru
     }
-    return -1; // Return -1 if stack is empty
+    return -1; 
 }
