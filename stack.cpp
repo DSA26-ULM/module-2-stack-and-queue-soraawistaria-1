bb264aafc40d5f8394e9f7d158bed1cfeda406e8
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-18 22:24:29 +0800

    sudah gin, guring ja
    udah nganu stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,28 +1,28 @@
 #include "stack.h"
 
 void init(Stack* s) { 
-    *s->top = -1;  //jari di posisi -1
+    s->top = &s->data[-1] ;  //jari di posisi -1
 }
 bool isEmpty(const Stack* s) {
-    return *s->top == -1;  //apakah kosong? (-1)
+    return s->top == &s->data[-1];  //apakah kosong? (-1)
 }
 bool isFull(const Stack* s) {
-    return *s->top == MAX - 1;  //apakah full? (99)
+    return s->top == &s->data[MAX];  //apakah full? (99)
 }
 void push(Stack* s, int value) {
     if (!isFull(s)) {  //kalau gak full
-        (*s->top)++;  
-        s->data[*s->top] = value;  //tambah elemen
+        (s->top)++;  
+        s->data[*(s->top)] = value;  //tambah elemen
     }
 }
 void pop(Stack* s) {
     if (!isEmpty(s)) {  //kalau gak kosong
-        (*s->top)--; 
+        (s->top)--;     // mundur indeks
     }
 }
 int peek(const Stack* s) {
     if (!isEmpty(s)) {  //kalau gak kosong
-        return s->data[*s->top];  //intip elemen terbaru
+        return s->data[*(s->top)];  //intip elemen terbaru
     }
     return -1; 
 }
