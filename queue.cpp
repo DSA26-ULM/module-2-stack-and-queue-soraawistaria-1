6943f7db3e4697aa5147f62ec347b141ecb7d985
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-21 14:05:49 +0800

    insyaaAllah benar
    no AI boss

--- queue.cpp
+++ queue.cpp
@@ -1,29 +1,40 @@
 #include "queue.h"
 
 void init(Queue* q) {
-
+    q->front = q->data;
+    q->rear = q->data + 1;
 }
 
 bool isEmpty(const Queue* q) {
-
+    return q->front == q->data;
 }
 
 bool isFull(const Queue* q) {
-
+    return q->rear == (q->data + (MAX -1));
 }
 
 void enqueue(Queue* q, int value) {
-
+    if (!isFull(q)) {
+        q->rear++;
+        *q->rear = value;
+    }
 }
 
 void dequeue(Queue* q) {
-
+    if (!isEmpty(q)) {
+        q->front++;
+    }
 }
 
 int front(const Queue* q) {
-
+    if(!isEmpty(q)){
+        return *(q->front);
+    }
+    return -1;
 }
 
 int back(const Queue* q) {
-    
+    if(!isEmpty(q)){
+        return *(q->rear);
+    }
 }
