c1ec21ece199a3ea02531973a526ff993808be6f
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-21 14:46:35 +0800

    hm gimana yah

--- prob2.cpp
+++ prob2.cpp
@@ -5,12 +5,16 @@ using namespace std;
 int main() {
     Queue q;
     init(&q);
-    int N, k, x;
+    int N, k, x, hasil;
 
     cin >> N >> k;
     for(int i = 0; i < N; i++) {
         cin >> x;
+        enqueue(&q, x);
+    }
 
+    for(int j = 0; j < k; j++){
         
     }
+
 }
