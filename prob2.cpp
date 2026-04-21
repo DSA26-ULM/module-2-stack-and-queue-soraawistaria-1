8bc25a74ba12611a1d4605b92f6217d9d54560e5
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-21 14:36:01 +0800

    inputnya dulu

--- prob2.cpp
+++ prob2.cpp
@@ -1,1 +1,16 @@
 #include "queue.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Queue q;
+    init(&q);
+    int N, k, x;
+
+    cin >> N >> k;
+    for(int i = 0; i < N; i++) {
+        cin >> x;
+
+        
+    }
+}
