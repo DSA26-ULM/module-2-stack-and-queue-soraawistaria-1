4260613297a2f28a50866bd74a223c618a0fbc61
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-21 14:50:45 +0800

    hmm kaya ini kah yo

--- prob2.cpp
+++ prob2.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     Queue q;
     init(&q);
-    int N, k, x, hasil;
+    int N, k, x;
 
     cin >> N >> k;
     for(int i = 0; i < N; i++) {
@@ -14,7 +14,10 @@ int main() {
     }
 
     for(int j = 0; j < k; j++){
-        
+        int hasil;
+        int nilai = front(&q);
+        hasil += nilai;
+        cout << hasil << " ";
     }
 
 }
