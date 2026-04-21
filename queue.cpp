20a841932b7188f267ceebaade63132b81a27eac
Amalia Soraya <amaliasoraya2109@gmail.com>
2026-04-21 13:13:26 +0800

    prepare

--- queue.cpp
+++ queue.cpp
@@ -1,1 +1,29 @@
 #include "queue.h"
+
+void init(Queue* q) {
+
+}
+
+bool isEmpty(const Queue* q) {
+
+}
+
+bool isFull(const Queue* q) {
+
+}
+
+void enqueue(Queue* q, int value) {
+
+}
+
+void dequeue(Queue* q) {
+
+}
+
+int front(const Queue* q) {
+
+}
+
+int back(const Queue* q) {
+    
+}
